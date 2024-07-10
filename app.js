24355b6e34a3206122c5efc50593ca3f04a6dee6
rajae yamoul <rajaeyamoul07@gmail.com>
2024-07-10 17:05:30 +0100

    Description des changements

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ function hide(){
     hover.classList.remove('active');
     modal.classList.remove('show');
 }
+function downloadVCard() {
+    var vcard = "BEGIN:VCARD\nVERSION:3.0\nFN:Mr. Mrs..\nTEL:0699999999\nEMAIL:yourmail@gmail.com\nEND:VCARD";
+    var blob = new Blob([vcard], { type: "text/vcard;charset=utf-8;" });
+    var link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.download = "contact.vcf";
+    link.click();
+}
 
 image.addEventListener('click', show);
 close.addEventListener('click', hide);
